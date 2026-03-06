54fd4e9f5ff98fbd84cbacf97b718ecfda3116dd
triplesium <shiss2001@outlook.com>
2026-03-06 16:11:40 +0800

    chore: update dependencies and add preprocessor flag

--- xmake.lua
+++ xmake.lua
@@ -2,8 +2,7 @@ add_rules("mode.debug", "mode.release")
 set_languages("c++23")
 set_warnings("all")
 
-set_runtimes("MD")
-add_requires("catch2", "stb", "glad", "lua", "tinyobjloader")
+add_requires("catch2", "stb", "glad", "lua", "tinyobjloader", "mikktspace")
 add_requires("glfw", {configs = {shared = false}})
 add_requires("imgui", {configs = {glfw = true, opengl3 = true}})
 
@@ -11,6 +10,8 @@ set_policy("check.auto_ignore_flags", false)
 
 local project_dir = os.scriptdir():gsub("\\", "/")
 add_cxxflags("-DFEI_ASSETS_PATH=\"" .. project_dir .. "/assets\"")
+
+add_cxxflags("cl::/Zc:preprocessor")
 
 includes("src")
 includes("samples")
